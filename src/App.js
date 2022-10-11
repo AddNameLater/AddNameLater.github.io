8285f68062f7654b9b5de91f2c889434a6916c0c
stevielammm <Lamsteven03@gmail.com>
2022-10-10 19:18:33 -0700

    updating steven

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <h1>Diet Tracker</h1>
+          <h1> Login Page</h1>
+          <div><input type="text" placeholder="user name" className="name"/></div>
+          <div><input type="text" placeholder="password" className="pass"/>
+          </div>
+          <div><button>Login</button></div>
+          <div>
+          </div>
+          <button>Forgot Password</button>
+          <button>Create Account</button>
+        </div>
+      </div>
     </div>
+
   );
 }
 

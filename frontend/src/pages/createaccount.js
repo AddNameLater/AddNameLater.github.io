f620c230b264a1639193b15d63dd5ee1c62dda94
datrinh469 <59327361+datrinh469@users.noreply.github.com>
2022-10-24 14:29:40 -0700

    Temp Placeholder while trying to fix venv

---
+++ frontend/src/pages/createaccount.js
@@ -0,0 +1,40 @@
+import React, { Component }  from 'react';
+import {useNavigate} from 'react-router-dom';
+import '../createaccount.css';
+
+function CreateAccountPage() {
+
+
+    const navigate = useNavigate();
+  
+    const navigateToDashboard = () => {
+      navigate('/dashboard');
+    };
+
+    const navigateToLogin = () => {
+        navigate('/');
+      };
+
+  return (
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <h1>Create Account</h1>
+          <div><input type="text" placeholder="first name" className="first name"/>
+          <input type="text" placeholder="last name" className="last name"/>
+          </div>
+          <div>
+          <input type="text" placeholder="email" className="email"/>
+          </div>
+          <div>
+          <input type="password" placeholder="password" className="passwrod"/>
+          </div>
+          <div><button onClick={navigateToLogin}>Create Account</button></div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default CreateAccountPage;

dcf4c6c14e320843ca83a866073e32a9f33683dc
dmrcogrn <dmrcogrn@gmail.com>
2022-12-01 00:55:53 -0800

    load dummy user data attempt plus some login code

--- backend/APIs.py
+++ backend/APIs.py
@@ -1,14 +1,16 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-loginf = LoginManager(app)
+loginu = LoginManager(app)
 
 uri = "mongodb+srv://Editor:Code9@diet-tracker.ncrahr5.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri)
+db = client.DietTracker
 
 @app.route('/', methods=['POST'])
 def login():
@@ -37,14 +39,17 @@ def dashboard():
 def editProfile():
     if request.method == "GET":
         #For response body, call GET functions for user data inside json variable
+        coll = db.Users
+        result = coll.find_one({"name": "User1"})
+        qresult = list(result.values())
         response_body = {
-            "userName" : "test",
-            "password" : "testPW",
-            "height" : 68,
-            "age" : 21,
-            "gender" : "Female",
-            "weight" : 155,
-            "flag" : 3
+            "userName" : str(qresult[1]),
+            "password" : str(qresult[3]),
+            "height" : int(qresult[4]),
+            "age" : int(qresult[9]),
+            "gender" : str(qresult[8]),
+            "weight" : int(qresult[5]),
+            "flag" : int(qresult[7])
         }
         return response_body
     elif request.method == "POST":
@@ -95,6 +100,48 @@ def userlogin(username):
         return qres
     elif request.method == "POST":
         return "test"   
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    coll = db.Users
+
+    username = request.form.get('userName')
+    password = request.form.get('password')
+
+    #will return if user under given username already exists
+    existCheck = coll.find_one({"name": username})
+    if existCheck:
+        return redirect(url_for('APIs.login')) #placeholder, ideally would redirect to blank signup form
+    
+    #add new user data to database
+    passhash = generate_password_hash(password) #generates a hash which is what the database will store
+    new_user = coll.insert_one({"name": username, "password": passhash}) #inserts new user into Users collection
+
+    #redirect user to login page after successful registration
+    return redirect(url_for('APIs.login'))
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    db = client.DietTracker
+    coll = db.Users
+    # login code
+    username = request.form.get('userName')
+    password = request.form.get('password')
+
+    #check enterted details
+    user = coll.find_one({"name": "User1"})
+    if not user: #if username doesn't match db
+        return redirect(url_for('APIs.login'))
+    uservals = list(user.values())
+    currentUserHash = uservals[2]
+    if not check_password_hash(currentUserHash, password): #if pass is wrong
+        return redirect(url_for('APIs.login'))
+
+    #passing both checks means user is verified
+    return 
+
+    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
